6be3f44a00ff722d5594af5f924532234afd2e31
DESKTOP-5O9FQKO\笑笑 <747486675@qq.com>
2020-11-11 17:53:24 +0800

    子应用

--- src/main.js
+++ src/main.js
@@ -3,6 +3,40 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// new Vue({
+//   render: h => h(App),
+// }).$mount('#app')
+const appOptions = {
+  el: '#microApp',
+  router,
+  store,
+  render: (h:any) => h(App)
+}
+
+// 支持应用独立运行、部署，不依赖于基座应用
+if (!(window as any).singleSpaNavigate) {
+  delete appOptions.el
+  new Vue(appOptions).$mount('#app')
+}
+
+// 基于基座应用，导出生命周期函数
+const vueLifecycle = singleSpaVue({
+  Vue,
+  appOptions
+})
+
+
+export function bootstrap (props: any) {
+  console.log('app1 bootstrap')
+  return vueLifecycle.bootstrap(() => {})
+}
+
+export function mount (props: any) {
+  console.log('app1 mount')
+  return vueLifecycle.mount(() => {})
+}
+
+export function unmount (props: any) {
+  console.log('app1 unmount')
+  return vueLifecycle.unmount(() => {})
+}
