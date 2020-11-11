711b9c1f8130e7e9e910dd38ed3fe712272d2ce6
中台-郭庆伍 <guo.qw@burgeon.cn>
2020-11-11 18:15:04 +0800

    增加基础路由

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,8 @@
+const path = require('path');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const path = require('path')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const resolve = dir => path.resolve(__dirname, dir)
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const resolve = (dir) => path.resolve(__dirname, dir);
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   // publicPath: '/',
@@ -14,9 +14,9 @@ module.exports = {
     // 导出umd格式的包，在全局对象上挂载属性package.name，基座应用需要通过这个全局对象获取一些信息，比如子应用导出的生命周期函数
     output: {
       // library的值在所有子应用中需要唯一
-      library: 'project',
-      libraryTarget: 'umd'
-    }
+      library: 'workbench',
+      libraryTarget: 'umd',
+    },
   },
   devServer: {
     proxy: {
