bb0632cd311ebb60013b54a92524877feceddae2
DESKTOP-5O9FQKO\笑笑 <747486675@qq.com>
2020-11-11 16:38:38 +0800

    子应用代码

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,52 @@ import network from './utils/network';
 import '@syman/ark-ui/dist/styles/ark-ui.css';
 import '@syman/ark-ui/dist/styles/bjIconfonts/iconfont.css';
 import './assets/css/index.less';
+import singleSpaVue from 'single-spa-vue';
 
 Vue.use(ArkUi);
 Vue.prototype.$network = network;
 
 Vue.config.productionTip = false;
 
-(window as any).vm = new Vue({
+// (window as any).vm = new Vue({
+//   router,
+//   store,
+//   render: (h) => h(App),
+// }).$mount('#app');
+
+
+const appOptions = {
+  el: '#microApp',
   router,
   store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: (h:any) => h(App)
+}
+
+// 支持应用独立运行、部署，不依赖于基座应用
+if (!(window as any).singleSpaNavigate) {
+  delete appOptions.el
+  new Vue(appOptions).$mount('#app')
+}
+
+// 基于基座应用，导出生命周期函数
+const vueLifecycle = singleSpaVue({
+  Vue,
+  appOptions
+})
+
+
+export function bootstrap (props: any) {
+  console.log('app1 bootstrap')
+  return vueLifecycle.bootstrap(() => {})
+}
+
+export function mount (props: any) {
+  console.log('app1 mount')
+  return vueLifecycle.mount(() => {})
+}
+
+export function unmount (props: any) {
+  console.log('app1 unmount')
+  return vueLifecycle.unmount(() => {})
+}
+
--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,19 @@ const resolve = dir => path.resolve(__dirname, dir)
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  publicPath: '/',
-  outputDir: 'dist', // 默认dist
+  // publicPath: '/',
+  // outputDir: 'dist', // 默认dist
   lintOnSave: false,
   productionSourceMap: false,
+  filenameHashing: false,
+  configureWebpack: {
+    // 导出umd格式的包，在全局对象上挂载属性package.name，基座应用需要通过这个全局对象获取一些信息，比如子应用导出的生命周期函数
+    output: {
+      // library的值在所有子应用中需要唯一
+      library: 'workbench',
+      libraryTarget: 'umd'
+    }
+  },
   devServer: {
     proxy: {
       '/p/cs': {
