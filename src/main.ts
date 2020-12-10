7026ac64db141d0ebfaa17e3e5e223855e2c1009
guoqingwu <guo.qw@burgeon.cn>
2020-12-10 13:22:44 +0800

    single-spa-1。0

--- src/main.ts
+++ src/main.ts
@@ -23,38 +23,37 @@ Vue.config.productionTip = false;
 
 
 const appOptions = {
-  // el: '#microApp',
+  el: '#microApp',
   router,
   store,
   render: (h:any) => h(App)
 }
-new Vue(appOptions).$mount('#app')
 
 // 支持应用独立运行、部署，不依赖于基座应用
-// if (!(window as any).singleSpaNavigate) {
-//   delete appOptions.el
-//   new Vue(appOptions).$mount('#app')
-// }
-//
-// // 基于基座应用，导出生命周期函数
-// const vueLifecycle = singleSpaVue({
-//   Vue,
-//   appOptions
-// })
-//
-//
-// export function bootstrap (props: any) {
-//   console.log('app1 bootstrap')
-//   return vueLifecycle.bootstrap(() => {})
-// }
-//
-// export function mount (props: any) {
-//   console.log('app1 mount')
-//   return vueLifecycle.mount(() => {})
-// }
-//
-// export function unmount (props: any) {
-//   console.log('app1 unmount')
-//   return vueLifecycle.unmount(() => {})
-// }
-//
+if (!(window as any).singleSpaNavigate) {
+  delete appOptions.el;
+  new Vue(appOptions).$mount('#app')
+}
+
+// 基于基座应用，导出生命周期函数
+const vueLifecycle = singleSpaVue({
+  Vue,
+  appOptions
+})
+
+
+export function bootstrap (props: any) {
+  console.log('app1 bootstrap')
+  return vueLifecycle.bootstrap(() => {})
+}
+
+export function mount (props: any) {
+  console.log('app1 mount')
+  return vueLifecycle.mount(() => {})
+}
+
+export function unmount (props: any) {
+  console.log('app1 unmount')
+  return vueLifecycle.unmount(() => {})
+}
+
