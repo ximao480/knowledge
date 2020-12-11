9c5993f155c531bc3a600b9289538c9a3b682702
843748862@qq.com <843748862@qq.com>
2020-12-11 09:47:15 +0800

    修改禅道三方登录

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,5 +2,5 @@ import network from './network';
 
 export const getTasks = (path:string,postObj?:Object) => network.post(path,postObj);  //获取待办，动态，知识库
 
-export const checkZenTao = (postObj?:Object,path='/p/cs/zenDaoCookie') => network.post(path,postObj);  //获取待办，动态，知识库
+export const checkZenTao = (postObj?:Object,path='/p/cs/zenDaoCookie') => network.get(path,postObj);  //获取待办，动态，知识库
 
