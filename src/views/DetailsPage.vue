a0edc6bf21d80e9643d4c619258e69166addd24a
xia.hj <xia.hj@burgeon.cn>
2021-01-19 15:35:48 +0800

    支持根据ID展开树并聚焦

--- src/views/DetailsPage.vue
+++ src/views/DetailsPage.vue
@@ -2,15 +2,14 @@
   <div class="DetailsPage">
     <!-- 目录 -->
     <div class="directory">
-     <tree @selectedTree='selectedTree' :treeDatas="treeDatas"></tree>
-      <div class="flex-box-resizer">
-      </div>
+      <tree @selectedTree="selectedTree" :treeDatas="treeDatas"></tree>
+      <div class="flex-box-resizer"></div>
     </div>
     <!-- 详情 -->
     <div class="details">
       <!-- loading -->
       <Spin fix v-if="detailsLoading">
-        <Icon type="ios-loading" size=18 class="demo-spin-icon-load"></Icon>
+        <Icon type="ios-loading" size="18" class="demo-spin-icon-load"></Icon>
         <div>Loading</div>
       </Spin>
       <document ref="document" :detail="documentation"></document>
@@ -18,58 +17,68 @@
   </div>
 </template>
 <script>
-import tree from './Tree/Tree'
-import document from '../components/document';
-import { getDirectory, getDocumentation } from '../utils/api';
-import DateUtil from '../utils/dateApi';
-import axios from 'axios';
-
+import tree from "./Tree/Tree";
+import document from "../components/document";
+import { getDirectory, getDocumentation } from "../utils/api";
+import DateUtil from "../utils/dateApi";
+import axios from "axios";
+// import jsonData from "../../static/js/ztree/treeData.json";
 export default {
-  components:{
+  components: {
     tree,
-    document
+    document,
   },
   data() {
     return {
-      treeDatas: [],  //文档目录数据
-      documentation:{},  //文档数据
-
-      detailsLoading: false,  //文档内容loading
-    }
+      treeDatas: [], //文档目录数据
+      documentation: {}, //文档数据
+      detailsLoading: false, //文档内容loading
+    };
   },
-  methods:{
-    selectedTree(selected){//处理点击输查询接口逻辑
+  methods: {
+    expandNode(){//根据ID展开当前节点并聚焦
+      const ID=290
+       window.zTree.expandNode(ID)
+    },
+    selectedTree(selected) {
+      //处理点击输查询接口逻辑
       // selected：当前点击节点数据
       window.cancle();
       getDocumentation({
         id: selected.ID,
-        type: selected.type
-      }).then(res => {
-        if(res && res.data.code === 0){
-          this.$refs.document.clearStatus()
-          res.data.data.updateTime =  new DateUtil(new Date(res.data.data.updateTime)).getDateDiff()
-          this.documentation = res.data.data
-        }
-      }).catch((err) => {
-      if (axios.isCancel(err)) {
-        console.log('Rquest canceled'); // 请求如果被取消，这里是返回取消的message
-      } else {
-        console.log(err);
-      }
-    });
+        type: selected.type,
+      })
+        .then((res) => {
+          if (res && res.data.code === 0) {
+            this.$refs.document.clearStatus();
+            res.data.data.updateTime = new DateUtil(
+              new Date(res.data.data.updateTime)
+            ).getDateDiff();
+            this.documentation = res.data.data;
+          }
+        })
+        .catch((err) => {
+          if (axios.isCancel(err)) {
+            console.log("Rquest canceled"); // 请求如果被取消，这里是返回取消的message
+          } else {
+            console.log(err);
+          }
+        });
     },
-    getTrees() { //获取文档目录
-      this.detailsLoading = true
-      getDirectory().then(res => {
-        this.detailsLoading = false
-        if(res.data.code === 0){
+    getTrees() {
+      //获取文档目录
+      // this.treeDatas = jsonData.data;
+      this.detailsLoading = true;
+      getDirectory().then((res) => {
+        this.detailsLoading = false;
+        if (res.data.code === 0) {
           this.treeDatas = res.data.data
         }
-      })
-    }
+      });
+    },
   },
   created() {
-    this.getTrees()
-  }
-}
+    this.getTrees();
+  },
+};
 </script>
--- src/views/Tree/ZtreeComponent.vue
+++ src/views/Tree/ZtreeComponent.vue
@@ -1,94 +1,109 @@
 <template>
   <div class="content_wrap">
+
     <div class="zTreeDemoBackground left">
-      <ul
-        id="treeDemo"
-        class="ztree"
-      />
+      <ul id="treeDemo" class="ztree" />
     </div>
   </div>
 </template>
 
 <script>
-  import { fuzzySearch } from '../../../static/js/ztree/fuzzysearch';
-  export default {
-    name: 'ZTree',
-    data() {
-      return {
-        inputValue: '',
-        setting: {
-          check: {
-            enable: false// checkbox
-          },
-          view: {
-            selectedMulti: false,
-            showIcon: true,
-            nameIsHTML: true,
-            dblClickExpand: false,
-            showLine: false,
-          },
-          callback: {
-            beforeClick: this.beforeClick,
-          },
-          edit: {
-            enable: false,
-            editNameSelectAll: false
-          },
-          data: {
-            key: {
-              children: 'children',
-              name: 'title',
-            },
-            simpleData: {
-              enable: true,
-              idKey: 'ID', // 树节点ID名称
-              pIdKey: 'PARENT_ID', // 父节点ID名称
-            }
-          },
+export default {
+  name: "ZTree",
+  data() {
+    return {
+      inputValue: "",
+      setting: {
+        check: {
+          enable: false, // checkbox
         },
-      };
-    },
-    watch: {
-      zNodes: {
-        handler() {
-          $.fn.zTree.init($('#treeDemo'), this.setting, this.zNodes);
+        view: {
+          selectedMulti: false,
+          showIcon: true,
+          nameIsHTML: true,
+          dblClickExpand: false,
+          showLine: false,
+          fontCss: this.getFontCss
         },
-        deep: true
-      },
-    },
+        callback: {
+          beforeClick: this.beforeClick,
+        },
+        edit: {
+          enable: false,
+          editNameSelectAll: false,
+        },
+        data: {
+          key: {
+            children: "children",
+            name: "title",
+            tId: "ID", // 树节点ID名称
+          },
+          simpleData: {
+            enable: true,
+            idKey: "ID", // 树节点ID名称
+            pIdKey: "pId", // 父节点ID名称
 
-    props: {
-      zNodes: {
-        type: Array,
-        default: () => []
+            tId: "ID", // 树节点ID名称
+          },
+        },
       },
+    };
+  },
+  watch: {
+    zNodes: {
+      handler() {
+        $.fn.zTree.init($("#treeDemo"), this.setting, this.zNodes);
+      },
+      deep: true,
     },
-    methods: {
-      beforeClick(treeId, treeNode) {
-			if (treeNode.isParent) {
-				return true;
-			} else {
-         this.$emit('clickTreeNode',treeNode);
-			}
-		},
+  },
+
+  props: {
+    zNodes: {
+      type: Array,
+      default: () => [],
     },
-    mounted() {
-      $.fn.zTree.init($('#treeDemo'), this.setting, this.zNodes);
-    }
-  };
+  },
+  methods: {
+    expandNode(ID) {//根据ID展开当前节点
+      //当前节点ID
+
+      var treeObj = $.fn.zTree.getZTreeObj("treeDemo");//获取zTree根节点
+      var node = treeObj.getNodeByParam("ID", ID);//根据ID获取子节点数据
+      const getParentNode = node.getParentNode();//根据子节点获取父节点
+      treeObj.expandNode(getParentNode, true, false, true); //需根据父节点ID进行展开
+      node.highlight = true;//设置高亮属性
+      treeObj.updateNode(node);//更新当前节点
+    },
+     getFontCss(treeId, treeNode) {
+       //当前节点增加highlight：true 则高亮显示
+			return (!!treeNode.highlight) ? {'font-weight': '500', background:'rgb(240 247 253)',width:'100%'} : {color:"red", "font-weight":"normal"};
+		 },
+    beforeClick(treeId, treeNode) {
+      if (treeNode.isParent) {
+        return true;
+      } else {
+        this.$emit("clickTreeNode", treeNode);
+      }
+    },
+  },
+  mounted() {
+    window.zTree=this
+    $.fn.zTree.init($("#treeDemo"), this.setting, this.zNodes);
+  },
+};
 </script>
 
 <style lang="less" >
-.content_wrap{
-
+.content_wrap {
 }
-.highlight{
-font-size: 14px;
+.highlight {
+  font-size: 14px;
 }
-.cancelNode{
-  background:transparent !important;
+.cancelNode {
+  background: transparent !important;
 }
-    /* #areaTree{
+/* #areaTree{
         border:1px solid #e5e5e5;    margin-bottom: 2px;border-radius: 4px;overflow: scroll;width: 300px;
     }
     .box-title{
