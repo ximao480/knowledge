341b266faf3f13b9b268145e348a9df70b8549ce
843748862@qq.com <843748862@qq.com>
2021-01-27 20:00:00 +0800

    样式修改

--- src/utils/waterMark.js
+++ src/utils/waterMark.js
@@ -40,7 +40,7 @@ export const createWatermark = (object) => {
   const setting = Object.assign({}, defaultSettings, object);
 
   const maxWidth = document.documentElement.offsetWidth;
-  const maxHeight = document.documentElement.offsetHeight;
+  const maxHeight = document.documentElement.offsetHeight - 30;
   const rowNumber = (maxWidth - 40 - 80) / setting.watermark_intervalWidth; // 横向个数
   const coumnNumber = (maxHeight - 40 - 80) / setting.watermark_intervalheight; // 纵向个数
 
